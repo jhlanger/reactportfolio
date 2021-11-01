83469e39a58c267ddaf57902e05b4099df9b6cb9
jhlanger <jonhenrylangerman@gmail.com>
2021-10-31 17:12:17 -0700

    nav update 2

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,7 +6,7 @@ function Navigation(props) {
     <div className="navigation">
       <nav className="navbar navbar-expand navbar-dark bg-dark">
         <div className="container">
-          <Link className="navbar-brand" to="/">
+          <Link className="navbar-brand" to="/reactportfolio">
             Jon Langerman
           </Link>
 
